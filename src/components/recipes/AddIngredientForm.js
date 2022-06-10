3ec700d039392057acc9f8ad675d6ed5b952891d
dohaisalive <dohaisalive@gmail.com>
2022-06-10 12:41:03 +0300

    add ingredient form

--- src/components/recipes/AddIngredientForm.js
+++ src/components/recipes/AddIngredientForm.js
@@ -16,65 +16,23 @@ const AddIngredientForm = () => {
   };
 
   return (
-    <div className="container">
+    <>
       <form action="action_page.php">
-        <div className="row">
-          <div className="col-25">
-            <label>ingredient</label>
-          </div>
-          <div className="col-75">
-            <input
-              onChange={handleChange}
-              type="text"
-              id="fname"
-              name="name"
-              placeholder="Your name.."
-            />
-          </div>
-        </div>
-        {/* <div className="row">
-          <div className="col-25">
-            <label>Last Name</label>
-          </div>
-          <div className="col-75">
-            <input
-              type="text"
-              id="lname"
-              name="lastname"
-              placeholder="Your last name.."
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label>Country</label>
-          </div>
-          <div className="col-75">
-            <select id="country" name="country">
-              <option value="australia">Australia</option>
-              <option value="canada">Canada</option>
-              <option value="usa">USA</option>
-            </select>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-25">
-            <label>Subject</label>
-          </div>
-          <div className="col-75">
-            {/* <textarea
-              id="subject"
-              name="subject"
-              placeholder="Write something.."
-              style="height:200px"
-            ></textarea>
-          </div>
-        </div>*/}
-        <div className="row">
+        <div className="add-ingredient">
+          <label>new ingredient</label>
+
+          <input
+            onChange={handleChange}
+            type="text"
+            id="fname"
+            name="name"
+            placeholder="Your ingredient..."
+          />
+
           <input type="submit" onClick={handleSubmit} value="add ingredient" />
         </div>
       </form>
-    </div>
+    </>
   );
 };
 
