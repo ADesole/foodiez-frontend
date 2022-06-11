765eca0cf170c5161297f7b8d88d6bfd82d651a3
noraalobaidi <nalobaidi_@hotmail.com>
2022-06-12 00:22:22 +0300

    update ingredients filter css

--- src/components/recipes/MyRecipes.js
+++ src/components/recipes/MyRecipes.js
@@ -3,21 +3,35 @@ import RecipesStore from "../../stores/recipeStore";
 import { observer } from "mobx-react";
 import AuthStore from "../../stores/AuthStore";
 import Home from "../Home";
+import IngredientFilterList from "../ingredients/IngredientFilterList";
 
 // get the recipe ids from user and compare them with recipe ids from all the recipes (recipeList)
 function RecipeList() {
   let recipeList;
   if (AuthStore.user) {
-    recipeList = RecipesStore.filteredReceipes.filter(
-      (recipe) => AuthStore.user._id === recipe.user
-    ).map((recipe) => <RecipeItem recipe={recipe} />);
+    recipeList = RecipesStore.filteredReceipes
+      .filter((recipe) => AuthStore.user._id === recipe.user)
+      .map((recipe) => <RecipeItem recipe={recipe} />);
   }
 
   return (
     <div style={{ display: "flex" }}>
       <Home />
-      <div className="recipe-list">{recipeList}</div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {" "}
+        <div
+          style={{
+            position: "relative",
+            marginTop: "30px",
+            marginLeft: "30px",
+          }}
+        >
+          <IngredientFilterList />
+        </div>
+        <div className="recipe-list">{recipeList}</div>
+      </div>
     </div>
   );
 }
+
 export default observer(RecipeList);
