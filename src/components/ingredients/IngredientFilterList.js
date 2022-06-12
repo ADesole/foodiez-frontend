9c02aec7c6cbc8216a6b75175510a46429957f71
Ali Akbar <Ali.A.Akbar98@hotmail.com>
2022-06-12 16:46:24 +0300

    Added drop down menu to filter recipe

--- src/components/ingredients/IngredientFilterList.js
+++ src/components/ingredients/IngredientFilterList.js
@@ -2,35 +2,48 @@ import ingredientsStore from "../../stores/IngredientsStore";
 import IngredientItem from "./IngredientItem";
 import recipeStore from "./../../stores/recipeStore";
 import { observer } from "mobx-react";
+import { MultiSelect } from "react-multi-select-component";
+import { useState } from "react";
 
 let checkedItems = [];
 
 function IngredientFilterList() {
-  const checkListAdd = (checkItem) => {
-    checkedItems.push(checkItem._id);
-    console.log(checkedItems);
-  };
-  const checkListRemove = (checkItem) => {
-    let foundIndex = checkedItems.findIndex((object) => object === checkItem);
-    checkedItems.splice(foundIndex, 1);
-  };
+  const [selected, setSelected] = useState([]);
+  let ingredientListDrop = ingredientsStore.ingredients.map((ingredient) => (
+    { label: ingredient.name, value: ingredient._id }
+  ));
+
+  // const checkListAdd = (checkItem) => {
+  //   checkedItems.push(checkItem._id);
+  //   console.log(checkedItems);
+  // };
+  // const checkListRemove = (checkItem) => {
+  //   let foundIndex = checkedItems.findIndex((object) => object === checkItem);
+  //   checkedItems.splice(foundIndex, 1);
+  // };
+
+
   const handleSubmitFilterWith = () => {
+    checkedItems = selected.map(ingredient => ingredient.value)
     console.log(checkedItems);
     recipeStore.recipesWIngredient(checkedItems);
   };
 
   const handleSubmitFilterWithout = () => {
+    checkedItems = selected.map(ingredient => ingredient.value)
     console.log(checkedItems);
     recipeStore.recipesWoIngredient(checkedItems);
   };
 
-  let ingredientList = ingredientsStore.ingredients.map((ingredient) => (
-    <IngredientItem
-      ingredient={ingredient}
-      checkListAdd={checkListAdd}
-      checkListRemove={checkListRemove}
-    />
-  ));
+  // let ingredientList = ingredientsStore.ingredients.map((ingredient) => (
+  //   <IngredientItem
+  //     ingredient={ingredient}
+  //     checkListAdd={checkListAdd}
+  //     checkListRemove={checkListRemove}
+  //   />
+  // ));
+
+  
   return (
     <>
       {/* Make the ingredient-list-filter scrollable */}
@@ -40,7 +53,14 @@ function IngredientFilterList() {
         >
           Filter by ingredients:{" "}
         </div>
-        <div className="ingredient-list-filter">{ingredientList}</div>
+        {/* <div className="ingredient-list-filter">{ingredientList}</div> */}
+        <pre>{JSON.stringify(selected.name)}</pre>
+      <MultiSelect
+        options={ingredientListDrop}
+        value={selected}
+        onChange={setSelected}
+        labelledBy="Select"
+        />
         <div className="Button-container">
           <button
             className="Ingredient-filter-button"
